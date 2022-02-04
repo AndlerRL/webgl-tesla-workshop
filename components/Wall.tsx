0f9396afde00aa29acc6fe99f892168b1cc3c344
Roberto Lucas <andre.rlucas@outlook.com>
2022-02-04 12:29:47 -0600

    :construction: walls texture

--- components/Wall.tsx
+++ components/Wall.tsx
@@ -1,34 +1,53 @@
-import type { MeshProps } from '@react-three/fiber'
+import { MeshProps, useLoader } from '@react-three/fiber'
 import React from 'react'
 import * as THREE from 'three'
 
 export const XWall: React.FC<MeshProps> = (props) => {
+  const wall_texture_diff = useLoader(THREE.TextureLoader, '/assets/brickwall/textures/sandstone_brick_wall_01_diff_2k.jpg')
+  const wall_texture_disp = useLoader(THREE.TextureLoader, '/assets/brickwall/textures/sandstone_brick_wall_01_disp_2k.png')
 
+  wall_texture_diff.wrapS = THREE.RepeatWrapping;
+  wall_texture_diff.wrapT = THREE.RepeatWrapping;
+  wall_texture_diff.repeat.set(10, 1)
+  wall_texture_disp.repeat.set(10, 1)
   return (
     <mesh {...props} receiveShadow castShadow>
       <boxBufferGeometry args={[0.25, 20, 100]} />
       <meshPhysicalMaterial
-        emissive="#f0f0f0"
         clearcoat={1}
         roughness={1}
         clearcoatRoughness={0.75}
-        side={THREE.BackSide}
+        displacementMap={wall_texture_disp}
+        map={wall_texture_diff}
       />
     </mesh>
   )
 }
 
 export const YWall: React.FC<MeshProps> = (props) => {
+  const wall_texture_diff = useLoader(
+    THREE.TextureLoader,
+    '/assets/brickwall/textures/sandstone_brick_wall_01_diff_2k.jpg'
+  )
+  const wall_texture_disp = useLoader(
+    THREE.TextureLoader,
+    '/assets/brickwall/textures/sandstone_brick_wall_01_disp_2k.png'
+  )
 
+
+  wall_texture_diff.wrapS = THREE.RepeatWrapping;
+  wall_texture_diff.wrapT = THREE.RepeatWrapping;
+  wall_texture_diff.repeat.set(10, 1)
+  wall_texture_disp.repeat.set(10, 1)
   return (
     <mesh {...props} receiveShadow castShadow>
       <boxBufferGeometry args={[100, 20, 0.25]} />
       <meshPhysicalMaterial
-        emissive="#f0f0f0"
         roughness={1}
         clearcoat={1}
         clearcoatRoughness={0.75}
-        side={THREE.BackSide}
+        displacementMap={wall_texture_disp}
+        map={wall_texture_diff}
       />
     </mesh>
   )
